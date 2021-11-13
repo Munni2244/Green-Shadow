6468d69f4244066bb360c4126ff4b0a10fb9d59a
Munni2244 <munnikhatun51544@gmail.com>
2021-11-13 15:20:05 +0600

    done

--- src/Pages/Home/Services/Products.js
+++ src/Pages/Home/Services/Products.js
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
 import HomeProducts from './HomeProducts';
 
 const Products = () => {
+    const {loading}=useAuth();
     const [products, setProducts]=useState([]);
     useEffect(()=>{
         fetch('http://damp-eyrie-28424.herokuapp.com/products')
         .then(res=> res.json())
         .then(data=>setProducts(data))
     },[])
+
+    if(loading){
+        return(
+            <div class="d-flex justify-content-center mt-4">
+            <div class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        )
+    }
     return (
         <div style={{marginTop:'100px'}} className="container text-center ">
              <h1>OuR <span style={{color:'rgb(250, 117, 139)'}}>ProDuctS</span></h1>
